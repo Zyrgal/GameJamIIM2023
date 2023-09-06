38126591292b09d94caa70203b9fcbf19180a921
Zyrgal <Valud64@hotmail.fr>
2023-09-06 17:17:44 +0200

    [ADD] Rules logic

--- Assets/Scripts/TestingWord.cs
+++ Assets/Scripts/TestingWord.cs
@@ -11,6 +11,8 @@ public class TestingWord : MonoBehaviour
     [SerializeField] string letterToSearch;
     [SerializeField] string wordToSearch;
 
+    public List<int> alreadyIndexChoosen;
+
     public void UI_ResetText()
     {
         textBool.text = null;
@@ -26,7 +28,12 @@ public class TestingWord : MonoBehaviour
         textBool.text = SeachForAWord(word, wordToSearch).ToString();
     }
 
-    private bool SeachForALetter(string wordToSeachIn, string letterToSeach)
+    public void UI_TestEnum()
+    {
+        textBool.text = GetRandomEnum<EmployeeAccesory>().ToString();
+    }
+
+    public static bool SeachForALetter(string wordToSeachIn, string letterToSeach)
     {
         if (wordToSeachIn.Contains(letterToSeach))
         {
@@ -42,5 +49,33 @@ public class TestingWord : MonoBehaviour
             return true;
         }
         return false;
+    }
+
+    public static T GetRandomEnum<T>()
+    {
+        System.Array A = System.Enum.GetValues(typeof(T));
+        int random = UnityEngine.Random.Range(0, A.Length);
+        T V = (T)A.GetValue(random);
+        return V;
+    }
+
+    public static void GetRandomEnum<T>(out T return1, out int return2,int excludedIndex = -1)
+    {
+        System.Array A = System.Enum.GetValues(typeof(T));
+
+        int choosenID = -1;
+        if (excludedIndex != -1)
+        {
+            choosenID = UnityEngine.Random.Range(0, A.Length);
+
+            while (choosenID == excludedIndex)
+            {
+                choosenID = UnityEngine.Random.Range(0, A.Length);
+            }
+        }
+
+        T V = (T)A.GetValue(choosenID);
+        return1 = V;
+        return2 = choosenID;
     }
 }
