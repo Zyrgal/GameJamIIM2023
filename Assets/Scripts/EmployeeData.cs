979b84fb6fb79072e3bc1ff0b97b2a80651e0e1f
Zyrgal <Valud64@hotmail.fr>
2023-09-07 16:27:48 +0200

    employee data, random head/cloth

--- Assets/Scripts/EmployeeData.cs
+++ Assets/Scripts/EmployeeData.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public enum EnumRule
 {
@@ -60,25 +61,87 @@ public class EmployeeData : MonoBehaviour
     public List<InputSymbol> combinaison2 = new List<InputSymbol>();
     public List<InputSymbol> combinaison3 = new List<InputSymbol>();
 
-    public string employeeLastname;
-    public string employeeFirstname;
-    public string employeeDescription;
-    public string employeeEntreprise;
-    public string employeeCloth;
-    public string employeeAccessory;
+    public string employeeLastname = null;
+    public string employeeFirstname = null;
+    public string employeeDescription = null;
+    public string employeeEntreprise = null;
+    public string employeeCloth = null;
+    public string employeeAccessory = null;
 
-    
+    [SerializeField] List<GameObject> everyWearedObject;
+    [SerializeField] List<GameObject> everyHead;
+
+    int combinaisonCount = 6;
 
     private void Start()
     {
+        RandomiseCombinaison(combinaison1);
+        RandomiseCombinaison(combinaison2);
+        RandomiseCombinaison(combinaison3);
+
         allCombinaisons.Add(combinaison1);
         allCombinaisons.Add(combinaison2);
         allCombinaisons.Add(combinaison3);
         SetEmployee();
+        SetEmployeeVisual();
+    }
+
+    public void RandomiseCombinaison(List<InputSymbol> combinaisonToRandomise)
+    {
+        combinaisonToRandomise.Clear();
+        
+        for (int i = 0; i < combinaisonCount; i++)
+        {
+            combinaisonToRandomise.Add(new InputSymbol());
+            System.Array A = System.Enum.GetValues(typeof(InputSymbol));
+            InputSymbol V = (InputSymbol)A.GetValue(UnityEngine.Random.Range(0, A.Length));
+            combinaisonToRandomise[i] = V;
+        }
     }
 
     private void SetEmployee()
     {
+        if (employeeLastname != null)
+        {
+            employeeLastname = DataList.instance.savedLastNameList[Random.Range(0, DataList.instance.savedLastNameList.Count)];
+        }
+        if (employeeFirstname != null)
+        {
+            employeeFirstname = DataList.instance.savedFirstNameList[Random.Range(0, DataList.instance.savedFirstNameList.Count)];
+        }
+        if (employeeDescription != null)
+        {
+            employeeDescription = DataList.instance.savedDescriptionsList[Random.Range(0, DataList.instance.savedDescriptionsList.Count)];
+        }
+        if (employeeEntreprise != null)
+        {
+            employeeEntreprise = DataList.instance.savedEntrepriseNameList[Random.Range(0, DataList.instance.savedEntrepriseNameList.Count)];
+        }
+        if (employeeCloth != null)
+        {
+            employeeCloth = DataList.instance.savedClothList[Random.Range(0, DataList.instance.savedClothList.Count)];
+        }
+        if (employeeAccessory != null)
+        {
+            employeeAccessory = DataList.instance.savedAccessoryList[Random.Range(0, DataList.instance.savedAccessoryList.Count)];
+        }
+    }
 
+    private void SetEmployeeVisual()
+    {
+        foreach (var item in everyWearedObject)
+        {
+            if (item.name == employeeCloth)
+            {
+                item.SetActive(true);
+            }
+
+            if (item.name == employeeAccessory)
+            {
+                item.SetActive(true);
+            }
+        }
+
+        everyHead[Random.Range(0, everyHead.Count)].SetActive(true);
     }
 }
