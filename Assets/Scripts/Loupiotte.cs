ec92a5e1f3ad6ea1992bb5ee45a746e356ad82e5
DisturbedBanana <90250325+DisturbedBanana@users.noreply.github.com>
2023-09-08 11:09:56 +0200

    [ADD] loupiotte

--- Assets/Scripts/Loupiotte.cs
+++ Assets/Scripts/Loupiotte.cs
@@ -4,11 +4,34 @@ using UnityEngine;
 
 public class Loupiotte : MonoBehaviour
 {
+    public static Loupiotte Instance;
+
     SpriteRenderer spriteRenderer;
+
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
+    }
 
     private void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
         spriteRenderer.color = Color.white;
     }
+
+    public void ChangeLoupiotteColor(Color color)
+    {
+        spriteRenderer.color = color;
+        StartCoroutine("ChangeColorCoroutine");
+    }
+
+    private IEnumerator ChangeColorCoroutine()
+    {
+        yield return new WaitForSeconds(3f);
+        spriteRenderer.color = Color.white;
+    }   
 }
