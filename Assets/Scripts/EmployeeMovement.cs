ba48ede29b046c8b27492b9f8c724090ec782e8b
M0uftchup <julien.barlet34@gmail.com>
2023-09-08 12:33:49 +0200

    emplloyee dead

--- Assets/Scripts/EmployeeMovement.cs
+++ Assets/Scripts/EmployeeMovement.cs
@@ -6,10 +6,24 @@ using UnityEngine;
 public class EmployeeMovement : MonoBehaviour
 {
     public Animator animator;
+    public Animator door_anim;
+    public GameObject doors;
 
+    private void Start()
+    {
+        door_anim = GameObject.Find("Doors").GetComponent<Animator>();
+        doors = door_anim.gameObject;
+    }
     public void MoveTo(Vector3 targetPos, bool moveascenc = false)
     {
         StartCoroutine(MoveToCoroutine(targetPos, moveascenc));
+    }
+
+    public void trigger_destroy()
+    {
+        Debug.Log("test " + this.gameObject.name);
+        Destroy(this.gameObject.GetComponent<EmployeeData>().sheetData.gameObject);
+        Destroy(this.gameObject);
     }
 
     private IEnumerator MoveToCoroutine(Vector3 targetPos, bool moveascenc = false)
@@ -23,23 +37,19 @@ public class EmployeeMovement : MonoBehaviour
             yield return new WaitForSeconds(0.01f);
         }
 
-        AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
-        if (moveascenc && !stateInfo.IsName("Yeet"))
-        {
-            //animator.SetTrigger("Yeet");
-        }
-
         if (moveascenc)
         {
+            animator.SetTrigger("Yeet");
+            doors.GetComponent<Door_shut>().employee = this.gameObject;
+            this.gameObject.name="willtakedoor";
             while (Vector3.Distance(transform.position, targetPos) > 0.1f)
             {
                 transform.position = Vector3.Lerp(pos, targetPos, t);
-                t += 0.01f;
+                t += 0.1f;
                 yield return new WaitForSeconds(0.01f);
             }
+            door_anim.SetTrigger("Close");
             //Attendre que la porte de l'ascensseur se ferme
-            Destroy(this.gameObject.GetComponent<EmployeeData>().sheetData.gameObject);
-            Destroy(this.gameObject);
         }
         else
         {
